9003ce1b30f9e63c9a8ce8cbf4cc1a4a5e90794e
Isac <isacsouzapedroso@gmail.com>
2025-07-01 20:43:19 -0300

    Feito visualização tela home/index;

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,10 +12,31 @@ import Header from '../components/Header';
  * @css
  */
 
+import '../assets/css/Index.css';
+import '../assets/css/fontawesome-free-6.7.2-web/css/all.min.css';
+
 export default function Home() {
     return (
         <>
             <Header />
+            <section className="hero">
+                <div className="hero-content">
+                    <h1>Bem-vindo ao IdeaFlow</h1>
+                    <p>Conectando empresas inovadoras a investidores comprometidos com o futuro</p>
+
+                    <form className="search-form">
+                        <label htmlFor="produto">Projeto:</label>
+                        <div className="input-group">
+                            <input
+                                type="text"
+                                id="produto"
+                                placeholder="Nome do produto"
+                            />
+                            <button type="button">Pesquisar</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
